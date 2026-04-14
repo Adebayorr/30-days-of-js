0fb0a0dff32c6254b00ddc146912de30a2aff1a6
Adebayorr <raheemadebayorr@gmail.com>
2026-04-14 14:44:16 +0100

    solution to day 22 challenge

--- day22/script.js
+++ day22/script.js
@@ -1,65 +1,96 @@
-document.body.style.display = 'flex'
-document.body.style.justifyContent = 'center'
-document.body.style.alignItems = 'center'
-document.body.style.width = '90vw'
-const container = document.querySelector('.container')
-container.style.width = '70vw'
-// container.style.height = '90vh' 
-console.log(container.style.width)
-container.style.display = 'flex'
-container.style.flexWrap = 'wrap'
-container.style.gap = '3px'
+import countries from "../others/countries.js"
+
+// Level 1
+
+// document.body.style.display = 'flex'
+// document.body.style.justifyContent = 'center'
+// document.body.style.alignItems = 'center'
+// const container = document.querySelector('.container')
+// container.style.width = '80vw'
+// container.style.display = 'flex'
+// container.style.justifyContent = 'space-between'
+// container.style.flexWrap = 'wrap'
+// container.style.gap = '6px'
 
 /*
-
-++Create a div container on HTML document and create 100 to 100 numbers dynamically and append to the container div.
+++Create a div container on HTML document and create 0 to 101 numbers dynamically and append to the container div.
 ++Even numbers background is green
 ++Odd numbers background is yellow
-++Prime numbers background is red
-
+++Prime numbers background is purple
 */
-let div;
-for (let i = 0; i < 100; i++) {
-    div = document.createElement('div')
-    let number = document.createElement('p')
-    number.innerText = i + 1
-    number.style.textAlign = 'center'
-    number.style.fontWeight = 'bold'
-    number.style.fontFamily = 'Roboto'
-    div.appendChild(number)
-    container.appendChild(div)
-    if (i%2 === 0) {
-        div.style.backgroundColor = 'green'
-    } else {
-        div.style.backgroundColor = 'yellow'
+
+function isPrime (num) {
+    if (num < 2) return false
+    if (num === 2) return true
+    let is_prime = true
+
+    for (let j = 2; j < num; j++) {
+            if (num % j === 0) {
+                // console.log(j)
+                is_prime = false
+                break
+            }
     }
-    div.style.height = '50px'
-    div.style.width = '19%'
-    div.style.color = '#ffffff'
-    container.appendChild(div)
+    
+    if (is_prime) return true
 }
 
-// function isPrime (number) {
-//     let products = []
-//     let result;
-//     for (let i = 1; i <= number; i++) {
-//         if (number % i === 0) {
-//             if (number <= 1) {
-//                 return false
-//             } else {
-//                 products.push(i)
-//             }   
-            
-//         }
-//     } 
-    
-//     if (products.length === 2) {
-//         result = products
-//     } else {
-//         result =  false
+
+
+// for (let i = 0; i <= 101; i++) {
+//     let div;
+//     div = document.createElement('div')
+//     let number = document.createElement('p')
+//     number.innerText = i
+//     number.style.textAlign = 'center'
+//     number.style.fontWeight = 'bold'
+//     number.style.fontFamily = 'Roboto'
+//     div.appendChild(number)
+//     container.appendChild(div)
+//     if (i%2 === 0) {
+//         div.style.backgroundColor = 'green'
 //     }
     
-//     return products.length
+//     if (i % 2 === 1) {
+//         div.style.backgroundColor = 'yellow'
+//     } 
+    
+//     if (isPrime(i)) {
+//         div.style.backgroundColor = 'purple'
+//     }
+//     div.style.height = '80px'
+//     div.style.fontSize = '2rem'
+//     div.style.fontFamily = 'verdana, san-serif'
+//     div.style.width = '16%'
+//     div.style.color = '#ffffff'
+//     div.style.display = 'flex'
+//     div.style.justifyContent = 'center'
+//     div.style.alignItems = 'center'
+//     container.appendChild(div)
 // }
 
-// console.log(isPrime(95))
+
+
+// Level 2
+const container = document.querySelector('.container')
+const headingOne = `<h1 id="title">World Countries List</h1>`
+const headingTwo = `<h2 id="sub-title">Total Numbers of countries: ${countries.length}</h2>`
+const paragraphOne =`<p class="text">#30DaysOfJavaScript: JavaScript DOM-Day-2</p>`
+const paragraphTwo = `<p class="text">Author: Adebayor</p>`
+
+container.insertAdjacentHTML('beforebegin', headingOne)
+container.insertAdjacentHTML('beforebegin', headingTwo)
+container.insertAdjacentHTML('beforebegin', paragraphOne)
+container.insertAdjacentHTML('beforebegin', paragraphTwo)
+
+
+
+
+container
+countries.forEach(country => {
+    let div = document.createElement('div')
+    div.textContent = country
+    div.setAttribute('class', 'country')
+    container.appendChild(div)
+    console.log(div)
+})
--- day6/script.js
+++ day6/script.js
@@ -116,12 +116,12 @@ for (let i = 0; i <= 100; i++) {
     let isPrime = true
 
     for (let j = 2; j < i; j++) {
-            if (i % j == 0) {
+            if (i % j === 0) {
                 isPrime = false
                 break
             }
     }
-    isPrime == true ? console.log(i) : i;
+    isPrime === true ? console.log(i) : i;
 }
 
 // 10

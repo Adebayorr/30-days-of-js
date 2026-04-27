1627a7df5f6147c6f354a7bc37274edec48b393b
Adebayorr <raheemadebayorr@gmail.com>
2026-04-27 12:08:00 +0100

    fix: add layout to index.html

--- day25/script.js
+++ day25/script.js
@@ -1,4 +1,7 @@
 import countries_data from "./data/countries_data.js";
+
+
+
 document.title = 'Day 25: Visuali..'
 
 function filterCountriesByPop () {
@@ -11,15 +14,39 @@ function filterCountriesByPop () {
 filterCountriesByPop()
 
 function filterCountriesByLang () {
-    let languages = {
 
-    }
+// USING MAP METHOD
 
-    countries_data.forEach(country => {
-        country.languages.forEach(lang => {
-            console.log(lang)
-        })
-    })
+    // let languages = new Map()
+    // countries_data.forEach(country => {
+        // country.languages.forEach( lang => {
+        //     if (languages.has(lang)) {
+        //         let currentLang = languages.get(lang)
+        //         languages.set(lang, currentLang + 1)
+        //     } else {
+        //         languages.set(lang, 1)
+        //     }
+
+        // })
+
+
+// USING JS OBJECT
+        const languages = {}
+        countries_data.forEach( country => {
+            
+            country.languages.forEach(lang => {
+            if (Object.hasOwn(languages, lang)) {
+                languages[lang] += 1
+            } else {
+                languages[lang] = 1
+            }
+
+            let mostSpoken = Object.entries(languages).sort((a, b) => b[1] - a[1]).slice(0, 10)
+            return mostSpoken
+
+})})
+
+    
 }
 
-filterCountriesByLang()
+console.log(filterCountriesByLang())

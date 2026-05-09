02a96a5c4aaaa9f00dc30e072f9eefea6ebdb93b
Adebayorr <raheemadebayorr@gmail.com>
2026-05-09 18:00:01 +0100

    feat: create the generateCountries and listLanguage function

--- day30/scripts/countries.js
+++ day30/scripts/countries.js
@@ -7,6 +7,7 @@ const filterCapitalButton = document.querySelector('.by-capital')
 const filterNameButton = document.querySelector('.by-name')
 const filterPopulationButton = document.querySelector('.by-population')
 
+const countriesDivContainer = document.querySelector('.countries')
 
 inputContainer.addEventListener('submit', (e) => e.preventDefault())
 
@@ -35,12 +36,62 @@ function filterCountries () {
 }
 
 inputElement.addEventListener('keyup', () => {
-    filterCountries()
+    countriesDivContainer.innerHTML = ''
+    const newCountries =  filterCountries()
+    generateCountries(newCountries)
 })
 
 filterCapitalButton.addEventListener('click', () => {
+    countriesDivContainer.innerHTML = ''
     filterByCapital = !filterByCapital
     console.log(filterByCapital);
-    filterCountries()
+    const newCountries =  filterCountries()
+    generateCountries(newCountries)
 })
 
+function generateCountries (countries) {
+
+    countries.forEach( country => {
+        const countryWrapper = document.createElement('div')
+        const countryFlag = document.createElement('img')
+        const countryName = document.createElement('h2')
+        const countryDetailsWrapper = document.createElement('div')
+        const countryCapital = document.createElement('p')
+        const countryLanguages = document.createElement('p')
+        const countryPopulation = document.createElement('p')
+
+        countryWrapper.classList.add('country')
+        countryFlag.classList.add('flag-image')
+        countryName.classList.add('country-name')
+
+
+        countryFlag.setAttribute('src', country.flag)
+        countryFlag.setAttribute('alt', `Flag of ${country.name}`)
+        countryName.textContent = country.name
+        countryCapital.textContent = country.capital
+        countryLanguages.textContent = listLanguages(country.languages)
+        
+
+        countriesDivContainer.appendChild(countryWrapper)
+        countryWrapper.append(countryFlag, countryName, countryDetailsWrapper)
+        countryDetailsWrapper.append(countryCapital, countryLanguages, countryPopulation)
+    })
+}
+
+
+function listLanguages (languages) {
+    let formatLanguages = '' 
+    let lastLang
+    if (languages.length === 1) {
+        formatLanguages = languages[0]
+        return formatLanguages
+    }
+    
+    if (languages.length > 1){
+        formatLanguages = languages.slice(0, languages.length - 1).join(' ')
+        lastLang = ` and ${languages[languages.length - 1]} `
+        return formatLanguages + lastLang
+    }
+}
+
+console.log(listLanguages(['Yoruba', 'Fulfulde', 'Igbo']))

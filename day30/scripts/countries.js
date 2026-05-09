1840043bf2a65cd9709e84d91050f1c7c25d4a0d
Adebayorr <raheemadebayorr@gmail.com>
2026-05-09 17:02:10 +0100

    feat: create the filterCountries function

--- day30/scripts/countries.js
+++ day30/scripts/countries.js
@@ -3,110 +3,44 @@ import countries_data from "../db/countries_data.js";
 const inputElement = document.querySelector('input')
 const inputContainer = document.querySelector('form')
 const filterButtons = document.querySelectorAll('.input-btn')
+const filterCapitalButton = document.querySelector('.by-capital')
+const filterNameButton = document.querySelector('.by-name')
+const filterPopulationButton = document.querySelector('.by-population')
+
 
 inputContainer.addEventListener('submit', (e) => e.preventDefault())
 
-let filteredByName = true
-let filteredByPop = true
-let filteredByCapital = true
+let ascendingByName = false
+let ascendingByCapital = false
 
-// filterButtons.forEach( button => {
-//     button.addEventListener('click', (e) => {
-//         e.preventDefault()
-//         console.log('clicked')
-//         if (button.textContent.toLowerCase() === 'name') {
-//             filteredByName = true
-//             filteredByCapital = false
+let filterByName = false
+let filterByCapital = true
 
-//         } 
-        
-//         if (button.textContent.toLowerCase() === 'capital') {
-//             filteredByName = false
-//             filteredByCapital = true
+function filterCountries () {
+    if ((!filterByCapital && !filterByName) || filterByName) {
 
-//         }
+        const newCountries = countries_data.filter( country => country.name
+        .toLowerCase().match(inputElement.value.toLocaleLowerCase()))
 
-//         console.log(
-//             `Pop: ${filteredByPop}, Name: ${filteredByName} Capital: ${filteredByCapital}`
-//         )
+        console.log(newCountries)
+        return newCountries
+    } else if (filterByCapital && !filterByName) {
+        const newCountries = countries_data.filter( country => country.capital)
+        .filter( country => country.capital
+        .toLowerCase().match(inputElement.value.toLocaleLowerCase()))
 
-// })
-    
-// })
-// console.log(filterButtons)
-
-// inputElement.addEventListener('keyup', (e) => {
-//     if (filteredByName && filteredByPop ) {
-//         let inputVal = new RegExp(inputElement.value, 'gi')
-//         console.log(inputVal)
-//         const filteredCountries = countries_data.filter(country => country.name.match(inputVal)).sort()
-//         console.log(filteredCountries)
-//         return filteredCountries
-//     } 
-//     else if (filteredByCapital) {
-//         let inputVal = new RegExp(inputElement.value, 'gi')
-//         const filteredCountries = countries_data.filter( country => country.capital)
-//         .filter(country => country.capital.match(inputVal))
-//         console.log(filteredCountries)
-//         return filteredCountries
-//     } else if (filteredByPop) {
-        
-//     }
-    
-
-//     // console.log(filteredCountries)
-// })
-
-
-function handleFiltering (
-    input = inputElement.value, 
-    filterCriteria = 'name'
-) {
-    const filteredCountries = countries_data.filter( country => country[filterCriteria])
-    .filter( country => country[filterCriteria].toLowerCase().match(inputElement.value.toLowerCase()))
-    return filteredCountries
+        console.log(newCountries)
+        return newCountries
+    }
 }
 
-// inputElement.addEventListener('keyup', (e) => {
-//     handleFiltering()
-// })
-
-
-
-filterButtons.forEach( button => {
-    if (button.textContent.toLowerCase() === 'name' && filteredByName) {
-        console.log('clicked');
-        
-            button.addEventListener('click', () => {
-            const newCountries = handleFiltering(inputElement.value, 'name')
-            console.log(newCountries, filteredByName)
-            filteredByName = !filteredByName
-        })
-    } else if (button.textContent.toLowerCase() === 'name' && !filteredByName ) {
-            const newCountries = handleFiltering(inputElement.value, 'name').sort( (a, b) => b.name < a.name)
-            console.log(newCountries, filteredByName)
-            filteredByName = !filteredByName
-    }
-        
-        
-    // if (button.textContent.toLowerCase() === 'capital') {
-    //         button.addEventListener('click', () => {
-    //         const newCountries = handleFiltering(inputElement.value, 'capital') 
-    //         console.log(newCountries)
-    //         })
-    // } else if (button.textContent.toLowerCase() === 'capital') {
-    //         button.addEventListener('click', () => {
-    //         const newCountries = handleFiltering(inputElement.value, 'capital') .reverse()
-    //         console.log(newCountries)
-    // })
-
-        
-    // } else if (button.textContent.toLowerCase() === 'population') {
-    //     console.log(button.textContent.toLowerCase());
-    //     button.addEventListener('click', () => {
-    //         const output = handleFiltering()
-    //     } )
-        
-    // }
-
+inputElement.addEventListener('keyup', () => {
+    filterCountries()
 })
+
+filterCapitalButton.addEventListener('click', () => {
+    filterByCapital = !filterByCapital
+    console.log(filterByCapital);
+    filterCountries()
+})
+

4d9630b09525f37d12a45c85f5a09b417f9e685b
Adebayorr <raheemadebayorr@gmail.com>
2026-05-12 21:11:43 +0100

    feat: add styling to header and form section

--- day30/scripts/countries.js
+++ day30/scripts/countries.js
@@ -6,6 +6,9 @@ const filterButtons = document.querySelectorAll('.input-btn')
 const filterCapitalButton = document.querySelector('.by-capital')
 const filterNameButton = document.querySelector('.by-name')
 const filterPopulationButton = document.querySelector('.by-population')
+const barChartElement = document.getElementById('.bar-chart')
+
+console.log(visualiser);
 
 const countriesDivContainer = document.querySelector('.countries')
 
@@ -14,21 +17,37 @@ inputContainer.addEventListener('submit', (e) => e.preventDefault())
 let ascendingByName = false
 let ascendingByCapital = false
 
-let filterByName = false
-let filterByCapital = true
+let filterBy = 'name'
 
-function filterCountries () {
-    if ((!filterByCapital && !filterByName) || filterByName) {
+function filterCountries (criteria) {
 
-        const newCountries = countries_data.filter( country => country.name
-        .toLowerCase().match(inputElement.value.toLocaleLowerCase()))
-
+    if (criteria === 'name') {
+        let newCountries
+        if (ascendingByName) {
+            newCountries = countries_data.filter( country => country.name
+            .toLowerCase().match(inputElement.value.toLocaleLowerCase()))
+            
+        } else {
+            newCountries = countries_data.filter( country => country.name
+            .toLowerCase().match(inputElement.value.toLocaleLowerCase())).reverse()
+        }
         console.log(newCountries)
         return newCountries
-    } else if (filterByCapital && !filterByName) {
-        const newCountries = countries_data.filter( country => country.capital)
-        .filter( country => country.capital
-        .toLowerCase().match(inputElement.value.toLocaleLowerCase()))
+
+    } else if (criteria  === 'capital') {
+
+        let newCountries 
+        if (ascendingByCapital)  {
+            newCountries = countries_data.filter( country => country.capital)
+            .filter( country => country.capital
+            .toLowerCase().match(inputElement.value.toLocaleLowerCase()))
+        } else {
+            newCountries = countries_data.filter( country => country.capital)
+            .filter( country => country.capital
+            .toLowerCase().match(inputElement.value.toLocaleLowerCase())).reverse()
+            
+        }
+        
 
         console.log(newCountries)
         return newCountries
@@ -37,16 +56,25 @@ function filterCountries () {
 
 inputElement.addEventListener('keyup', () => {
     countriesDivContainer.innerHTML = ''
-    const newCountries =  filterCountries()
+    const newCountries =  filterCountries(filterBy)
     generateCountries(newCountries)
 })
 
 filterCapitalButton.addEventListener('click', () => {
     countriesDivContainer.innerHTML = ''
-    filterByCapital = !filterByCapital
-    console.log(filterByCapital);
-    const newCountries =  filterCountries()
+    filterBy = 'capital'
+    const newCountries =  filterCountries(filterBy)
     generateCountries(newCountries)
+    ascendingByCapital = !ascendingByCapital
+})
+
+filterNameButton.addEventListener('click', () => {
+    console.log('clicked');
+    filterBy = 'name'
+    countriesDivContainer.innerHTML = ''
+    const newCountries =  filterCountries(filterBy)
+    generateCountries(newCountries)
+    ascendingByName = !ascendingByName
 })
 
 function generateCountries (countries) {
@@ -95,3 +123,29 @@ function listLanguages (languages) {
 }
 
 console.log(listLanguages(['Yoruba', 'Fulfulde', 'Igbo']))
+
+
+
+const totalPopulation = countries_data.reduce((acc, curr) => acc + curr.population, 0)
+console.log(totalPopulation)
+
+
+// function visualiseCountries (countries, byWhat) {
+
+//     const countryName = document.createElement('p')
+//     const countryBar = document.createElement('div')
+//     const countryData = document.createElement('p')
+
+
+//     if (byWhat = 'population') {
+//         countryName.textContent = 
+//     }
+
+//     if (byWhat = 'languages') {
+
+//     }
+    
+
+
+
+// }
